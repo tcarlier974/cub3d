3663bc5c87dc2a4a5638441ba8d50147da7ddc09
Tristan Carlier <carliertiti97459@gmail.com>
2025-11-11 00:56:06 +0100

    Revert "Merge branch 'Ivan'"
    This reverts commit 1eac1b0e2cf25b02154bccfc164e9b90aeb44aa7, reversing
    changes made to 9fa14ea8f3ba80570c8639824c1f33da70497ac7.

--- src/main.c
+++ src/main.c
@@ -3,87 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
+/*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 19:31:49 by tcarlier          #+#    #+#             */
-/*   Updated: 2025/11/08 18:43:08 by igilbert         ###   ########.fr       */
+/*   Updated: 2025/10/30 19:35:26 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/include.h"
 
-void	mlx_shit(t_cube *cube)
-{
-	raycast(cube);
-	mlx_put_image_to_window((*cube).mlx, (*cube).win, (*cube).img.img, 0, 0);
-	mlx_do_key_autorepeaton((*cube).mlx);
-	mlx_hook((*cube).win, 17, 0, ft_mlx_loop_end, cube);
-	mlx_hook((*cube).win, 2, 1L << 0, key_hook_press, cube);
-	mlx_hook((*cube).win, 3, 1L << 1, key_release_hook, cube);
-	mlx_loop_hook((*cube).mlx, update_game_state, cube);
-	mlx_loop((*cube).mlx);
-}
-
-int	main_messages_one(int code, int j, char **av, t_cube *cube)
-{
-	if (code == 1)
-	{
-		if (count_lines(av[1]) <= 0 || count_max_col(av[1]) <= 0)
-		{
-			fprintf(stderr, "Invalid map file: %s\n", av[1]);
-			return (0);
-		}
-	}
-	else
-	{
-		if (!validate_map_closed(cube))
-		{
-			fprintf(stderr, "Error: non la map est pas bien fermée :3\n");
-			while (j < (*cube).map_height)
-			{
-				free((*cube).map[j]);
-				j++;
-			}
-			free((*cube).map);
-			return (0);
-		}
-		printf("Carte chargée:\n");
-	}
-	return (1);
-}
-
-void	main_print_map(int y, t_cube *cube)
-{
-	while (y < (*cube).map_height)
-	{
-		if ((*cube).map[y])
-			printf("[%d] %s\n", y, (*cube).map[y]);
-		y++;
-	}
-}
-
 int	main(int ac, char **av)
 {
 	t_cube	cube;
-	int		i;
+	int		i = 0;
 
-	i = 0;
 	if (ac != 2)
 	{
 		fprintf(stderr, "Usage: %s <map_file>\n", av[0]);
 		return (1);
 	}
-	if (!main_err_mes(1, 0, av, &cube))
+	if (count_lines(av[1]) <= 0 || count_max_col(av[1]) <= 0)
+	{
+		fprintf(stderr, "Invalid map file: %s\n", av[1]);
 		return (1);
+	}
 	init_cube(&cube, av[1]);
 	recup_texture(&cube, av[1]);
 	init_map(&cube.map, av[1], &cube);
-	if (!main_err_mes(2077, 0, av, &cube))
+	if (!validate_map_closed(&cube))
+	{
+		fprintf(stderr, "Error: non non non la map est pas bien fermée :3\n");
+		for (int j = 0; j < cube.map_height; j++)
+			free(cube.map[j]);
+		free(cube.map);
 		return (1);
-	main_print_map(0, &cube);
-	printf("Player initialized at position: %.2f, %.2f\n",
-		cube.player_x, cube.player_y);
+	}
+	printf("Carte chargée:\n");
+	for (int y = 0; y < cube.map_height; y++) {
+		if (cube.map[y])
+			printf("[%d] %s\n", y, cube.map[y]);
+	}
+	printf("Player initialized at position: %.2f, %.2f\n", cube.player_x, cube.player_y);
 	printf("Direction: (%.2f, %.2f)\n", cube.dir_x, cube.dir_y);
-	mlx_shit(&cube);
+	raycast(&cube);
+	mlx_put_image_to_window(cube.mlx, cube.win, cube.img.img, 0, 0);
+	mlx_do_key_autorepeaton(cube.mlx);
+	mlx_hook(cube.win, 17, 0, ft_mlx_loop_end, &cube);
+	mlx_hook(cube.win, 2, 1L << 0, key_hook_press, &cube);
+	mlx_hook(cube.win, 3, 1L << 1, key_release_hook, &cube);
+	mlx_loop_hook(cube.mlx, update_game_state, &cube);
+	mlx_loop(cube.mlx);
 	return (0);
 }
--- src/utils.c
+++ src/utils.c
@@ -3,81 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: igilbert <igilbert@student.42perpignan.    +#+  +:+       +#+        */
+/*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 12:00:00 by tcarlier          #+#    #+#             */
-/*   Updated: 2025/11/08 18:46:03 by igilbert         ###   ########.fr       */
+/*   Updated: 2025/08/06 16:30:30 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/include.h"
 
-void	count_lines_loop(char **line, int *j, int fd)
-{
-	while (*j < 6)
-	{
-		while (*line[0] == '\n')
-		{
-			free(*line);
-			*line = get_next_line(fd);
-		}
-		if (*line[0] != '\n' && *line[0] != '\0')
-		{
-			*j++;
-		}
-		free(*line);
-		*line = get_next_line(fd);
-	}
-}
-
 int	count_lines(const char *file_path)
 {
-	int		fd;
-	char	*line;
-	int		count;
-	int		j;
+    int     fd;
+    char    *line;
+    int     count;
+	int j = 0;
 
-	j = 0;
-	count = 0;
-	fd = open(file_path, O_RDONLY);
-	if (fd < 0)
-		return (0);
-	line = get_next_line(fd);
-	count_lines_loop(&line, &j, fd);
-	while (line)
+    count = 0;
+    fd = open(file_path, O_RDONLY);
+    if (fd < 0)
+        return (0);
+    line = get_next_line(fd);
+		while (j < 6)
 	{
-		count++;
+		while(line[0] == '\n')
+		{
+			free(line);
+			line = get_next_line(fd);
+		}
+		if (line[0] != '\n' && line[0] != '\0')
+		{
+			j++;
+		}
 		free(line);
 		line = get_next_line(fd);
 	}
-	close(fd);
-	return (count);
-}
-
-int	count_max_col_setup(const char *file_path, int *fd, char **line)
-{
-	*fd = open(file_path, O_RDONLY);
-	if (*fd < 0)
-		return (0);
-	*line = get_next_line(*fd);
-	if (!*line)
-	{
-		close(*fd);
-		return (0);
-	}
-	return (1);
+    while (line)
+    {
+        count++;
+        free(line);
+        line = get_next_line(fd);
+    }
+    close(fd);
+    return (count);
 }
 
 int	count_max_col(const char *file_path)
 {
-	int		fd;
-	char	*line;
-	int		max;
-	int		i;
+    int     fd;
+    char    *line;
+    int     max;
+    int     i;
 
-	if (!count_max_col_setup(file_path, &fd, &line))
-		return (0);
-	max = 0;
+    fd = open(file_path, O_RDONLY);
+    if (fd < 0)
+        return (0);
+    line = get_next_line(fd);
+    if (!line)
+    {
+        close(fd);
+        return (0);
+    }
+    max = 0;
 	while (line)
 	{
 		i = 0;
@@ -88,7 +75,7 @@ int	count_max_col(const char *file_path)
 		free(line);
 		line = get_next_line(fd);
 	}
-	free(line);
-	close(fd);
-	return (max);
+    free(line);
+    close(fd);
+    return (max);
 }
